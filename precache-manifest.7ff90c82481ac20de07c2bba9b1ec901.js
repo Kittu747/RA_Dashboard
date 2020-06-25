810b9b5b17730f16a8e940675d962bd809409acf
unknown <chaitanyawithkittu747@gmail.com>
2020-06-25 12:16:10 +0530

    Updates

---
+++ precache-manifest.7ff90c82481ac20de07c2bba9b1ec901.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46847c33f48c5783dc19c7d2184b6435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2901b346328bd45dc53",
+    "url": "/static/css/2.b9bf8db6.chunk.css"
+  },
+  {
+    "revision": "eaa214242160ca3ac0bd",
+    "url": "/static/css/main.5ae4150a.chunk.css"
+  },
+  {
+    "revision": "d2901b346328bd45dc53",
+    "url": "/static/js/2.1c94a2f3.chunk.js"
+  },
+  {
+    "revision": "28c7584cfc5cb34e0371136de2d12cd7",
+    "url": "/static/js/2.1c94a2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa214242160ca3ac0bd",
+    "url": "/static/js/main.7923578f.chunk.js"
+  },
+  {
+    "revision": "3a9118e9bde3366e7356",
+    "url": "/static/js/runtime-main.fc2d4af3.js"
+  },
+  {
+    "revision": "634265d274b0c08ebfedb278e670b7bd",
+    "url": "/static/media/avatar-3.634265d2.jpg"
+  },
+  {
+    "revision": "c60dbb750b4b6a59652ce6be58ab9c52",
+    "url": "/static/media/college_icon.c60dbb75.png"
+  },
+  {
+    "revision": "b05f56a48c6728d0c51d1452ad5ff336",
+    "url": "/static/media/gandhi.b05f56a4.png"
+  },
+  {
+    "revision": "46c365a2978b0a92fa98bf2ea760247e",
+    "url": "/static/media/grayarrow.46c365a2.PNG"
+  }
+]);
